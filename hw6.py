fad3026b5845d9328dbedc525ccfae829c37b614
김근우 <128760363+Kim-geun-woo@users.noreply.github.com>
2024-04-22 17:53:02 +0900

    multiplication tables

---
+++ hw6.py
@@ -0,0 +1,10 @@
+def display_multiplication_table(n):
+        for i in range (1, 10):
+            for j in range(n, n+4):
+                print(f'{j} x {i} = {j * i:2d}', end = '\t')
+            print()
+     
+
+display_multiplication_table(2)
+print()
+display_multiplication_table(6)
